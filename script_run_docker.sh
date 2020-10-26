886ee0f3d2b1d5a153d8c14dd4cd9b43f6e98b3f
gerome12 <gerome.pasquier@gmail.com>
2020-10-26 11:04:53 +0100

    use keycloak in docker

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -4,7 +4,7 @@ cd kheops
 docker-compose down -v
 docker-compose up -d
 cd ..
-sleep 45
+sleep 60
 docker ps -a
 
 node_modules/.bin/newman run postman/kheops-Get_tokens.json -e postman/kheops_localhost.postman_environment.json -g postman/Workspace.postman_globals.json --export-globals postman/Workspace.postman_globals.json
