936e974fe2d44e46b17b4b575bd002ab68731784
gerome12 <gerome.pasquier@gmail.com>
2020-10-27 08:36:14 +0100

    test script

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -5,6 +5,20 @@ docker-compose down -v
 docker-compose up -d
 cd ..
 sleep 60
+
+docker_name "kheopsreverseproxy" "pacsauthorizationproxy" "kheopsauthorization" "pacsarc" "pacspostgres" "keycloak" "kheopsdicomwebproxy"\
+            "kheopszipper" "kheopsui" "kheopspostgres" "ldap"
+
+while [true]
+do
+   for dn in [docker_name]
+   do
+      echo docker logs --tail 1 $dn
+   done
+   break
+done
+
+
 docker ps -a
 
 node_modules/.bin/newman run postman/kheops-Get_tokens.json -e postman/kheops_localhost.postman_environment.json -g postman/Workspace.postman_globals.json --export-globals postman/Workspace.postman_globals.json
