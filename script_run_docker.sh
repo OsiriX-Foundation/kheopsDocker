d09a8dd54ecbf9991d2bfb186a0ba32fe40b9f21
gerome12 <gerome.pasquier@gmail.com>
2020-10-27 15:09:34 +0100

    test

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -5,36 +5,7 @@ docker-compose down -v
 docker-compose up -d
 cd ..
 
-docker_name=("kheopsreverseproxy" "pacsauthorizationproxy" "kheopsauthorization" "pacsarc" "pacspostgres" "keycloak" "kheopsdicomwebproxy"\
-            "kheopszipper" "kheopsui" "kheopspostgres" "ldap")
-
-docker_logs=("" "" "" "" "" "" "" "" "" "" "")
-
-while true;
-do
-   i=0
-   flag=false
-   for dn in "${docker_name[@]}"
-   do
-      old_log=${docker_logs[$i]}
-      log=$(docker logs --tail 1 $dn) > /dev/null 2>&1
-
-      if [ "$old_log" != "$log" ]; then 
-         flag=true
-      fi
-
-      docker_logs[i]=$log
-      i=$((i+1))
-   done
-
-   if [ $flag = true ];
-   then
-      echo "waiting 10 secondes..."
-      sleep 10
-   else
-    break
-   fi
-done
+./script_wait_docker_start > /dev/null
 
 docker ps -a
 
