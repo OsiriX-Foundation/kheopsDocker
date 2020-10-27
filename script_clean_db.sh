25e3fd2ec565208af72d5a5798a5db862a1ee582
gerome12 <gerome.pasquier@gmail.com>
2020-10-27 14:09:11 +0100

    Update script_clean_db.sh

--- script_clean_db.sh
+++ script_clean_db.sh
@@ -2,13 +2,13 @@
 
 docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "SELECT * FROM albums"
 
-docker exec -it kheopspostgres pg_dump -U kheopsuser -s -f kheops.dump kheops
+docker exec -it kheopspostgres pg_dump -U kheopsuser -Fs -s -f kheops.dump kheops
 
 docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "UPDATE pg_database SET datallowconn = 'false' WHERE datname = 'kheops'"
-docker exec -it kheopspostgres dropdb -U kheopsuser kheops -f
+docker exec -it kheopspostgres dropdb -U kheopsuser kheops
 docker exec -it kheopspostgres createdb -U kheopsuser kheops
+docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "UPDATE pg_database SET datallowconn = 'true' WHERE datname = 'kheops'"
 docker exec -it kheopspostgres pg_restore -U kheopsuser -v -d kheops kheops.dump
 
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "UPDATE pg_database SET datallowconn = 'true' WHERE datname = 'kheops'"
 
 docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "SELECT * FROM albums"
