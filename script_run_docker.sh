596da41d570aeefecf65bb98fd6050487d6c00f3
gerome12 <gerome.pasquier@gmail.com>
2020-10-27 11:06:32 +0100

    Update script_run_docker.sh

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -17,7 +17,7 @@ do
    for dn in "${docker_name[@]}"
    do
       old_log=${docker_logs[$i]}
-      log=$(docker logs --tail 1 $dn)
+      log=$(docker logs --tail 1 $dn) &
 
       if [ "$old_log" != "$log" ]; then 
          flag=true
