730f52dfcefcd2897f95fa5b8153258e179fcd7e
gerome12 <gerome.pasquier@gmail.com>
2020-10-27 13:37:38 +0100

    Update script_clean_db.sh

--- script_clean_db.sh
+++ script_clean_db.sh
@@ -2,7 +2,7 @@
 
 docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "SELECT * FROM albums"
 
-docker exec -it kheopspostgres pg_dump -U kheopsuser -v -Fc -s -f kheops.dump kheops
+docker exec -it kheopspostgres pg_dump -U kheopsuser -s -f kheops.dump kheops
 docker exec -it kheopspostgres dropdb -U kheopsuser kheops
 docker exec -it kheopspostgres createdb -U kheopsuser kheops
 docker exec -it kheopspostgres pg_restore -U kheopsuser -v -d kheops kheops.dump
