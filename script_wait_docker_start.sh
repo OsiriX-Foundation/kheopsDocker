cd508f12e3b7b6eefb5b27bba5bd042b026b0e1b
gerome12 <gerome.pasquier@gmail.com>
2020-10-28 08:59:55 +0100

    Update script_wait_docker_start.sh

--- script_wait_docker_start.sh
+++ script_wait_docker_start.sh
@@ -13,7 +13,7 @@ do
    for dn in "${docker_name[@]}"
    do
       old_log=${docker_logs[$i]}
-      log=$(docker logs --tail 1 $dn > /dev/null)
+      log=$(docker logs --tail 1 $dn)
 
       if [ "$old_log" != "$log" ]; then 
          flag=true
