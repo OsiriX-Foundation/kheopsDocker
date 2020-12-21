62b567d8e8bfb3b08be1e2238a2a711c340f7546
gerome12 <gerome.pasquier@gmail.com>
2020-12-21 14:22:52 +0100

    Update script_clean_db.sh

--- script_clean_db.sh
+++ script_clean_db.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM event_series" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM events" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM report_providers" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM album_series" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM album_user" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_trigger_series" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_attempts" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_triggers" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhooks" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM capabilities" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM series" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM studies" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM users" > /dev/null
-docker exec -it kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM albums" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM event_series" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM events" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM report_providers" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM album_series" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM album_user" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_trigger_series" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_attempts" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhook_triggers" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM webhooks" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM capabilities" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM series" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM studies" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM users" > /dev/null
+docker exec -i kheopspostgres psql -Ukheopsuser kheops -c "DELETE FROM albums" > /dev/null
