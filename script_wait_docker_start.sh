d6a3b038f17cfa5ff137bce2e28125c55763fa0f
gerome12 <gerome.pasquier@gmail.com>
2020-12-22 09:03:49 +0100

    Update script_wait_docker_start.sh

--- script_wait_docker_start.sh
+++ script_wait_docker_start.sh
@@ -1,33 +1,3 @@
 #!/bin/bash
 
-
-docker_name=("kheopsreverseproxy" "pacsauthorizationproxy" "kheopsauthorization" "pacsarc" "pacspostgres" "keycloak" "kheopsdicomwebproxy"\
-            "kheopszipper" "kheopsui" "kheopspostgres" "ldap")
-
-docker_logs=("" "" "" "" "" "" "" "" "" "" "")
-
-while true;
-do
-   i=0
-   flag=false
-   for dn in "${docker_name[@]}"
-   do
-      old_log=${docker_logs[$i]}
-      log=$(docker logs --tail 1 $dn)
-
-      if [ "$old_log" != "$log" ]; then 
-         flag=true
-      fi
-
-      docker_logs[i]=$log
-      i=$((i+1))
-   done
-
-   if [ $flag = true ];
-   then
-      echo "waiting 10 secondes..."
-      sleep 10
-   else
-    break
-   fi
-done
+sleep 180
